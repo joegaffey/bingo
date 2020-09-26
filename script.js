e1c27676911f219551d1d202a4574bf2e32d3c7a
Glitch (blingo) <none>
2020-09-26 13:17:34 +0000

    🏢😹 Updated with Glitch

--- script.js
+++ script.js
@@ -2,7 +2,21 @@
 
 let maxNum = 40;
 
-let strings = Array.from({length: 24}, () => Math.floor(Math.random() * maxNum));
+// Nope - Can contain duplicates
+// let nums = Array.from({length: 24}, () => Math.floor(Math.random() * maxNum)); 
+
+// Nope - Can contain 0
+// let nums = [...Array(maxNum).keys()].sort(() => { 
+//     return Math.random() - 0.5; 
+// });
+
+const range = (start, end, step) => {
+  return Array.from(Array.from(Array(Math.ceil((end-start)/step)).keys()), x => start+ x*step);
+}
+
+let nums = range(1, maxNum, 1).sort(() => { // Can contain 0
+    return Math.random() - 0.5; 
+});
 
 
 document.querySelectorAll('#card tbody td').forEach((space, spaceIndex) => {
@@ -11,9 +25,9 @@ document.querySelectorAll('#card tbody td').forEach((space, spaceIndex) => {
   });
   
   // Skip "Safe Space"
-  if (spaceIndex == 12) {
-    return;
-  };
+  // if (spaceIndex == 12) {
+  //   return;
+  // };
   
-  space.innerText = strings.pop();
+  space.innerText = nums.pop();
 });
